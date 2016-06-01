57f0a5c4a38d912bb8248a837f3214d98b2ac607
Christopher <chan42988@gmail.com>
2016-06-01 19:20:32 -0400

    beautifying form and getting lightbox to work

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,11 +1,34 @@
+// $(document).ready(function(){
+
+// 	$('#signup-toggle-button').on('click', function(){
+// 		$('body').append('<div id="shade"></div>');
+// 		$('body').append('#sessionz');
+
+
+// 		$('#shade').on('click', function(){
+// 			$('#shade').remove();
+// 			$('#light-image').remove();
+// 		})
+
+
+// 		$('#light-image').on('click', function(){
+// 			$('#shade').remove();
+// 			$('#light-image').remove();
+// 		})
+// 	})
+// })
+
 $(document).ready(function(){
+	$('#signup-toggle-button').click(function(){
+			$('.sessionz').css('display', 'block');
+			$('.backdrop').css('display', 'block');
+			$('.backdrop').css('opacity', '.6');
+	})
 
-  $('#signup-toggle-button').click(function(){
-    $('#sessionz').show();
-  });
+	$('.backdrop').click(function(){
+			$('.sessionz').css('display', 'none');
+			$('.backdrop').css('display', 'none');
+	})
 
-  $('#sessionz').click(function(){
-    $('#sessionz').hide();
-  });
 
 });
