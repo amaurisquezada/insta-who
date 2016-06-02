1ced0025b25955d67da351039c68e41d4aee591d
Amauris Quezada <Amauris.Quezada@gmail.com>
2016-06-02 17:26:48 -0400

    fine-tuning class attributes and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,111 +1,119 @@
 User.create(email: "aq@gmail.com", username: "aq123", password: "amauris", password_confirmation: "amauris")
 User.where(username: "aq123").first.games.create(name: "my_first_game")
-Game.where(name: "my_first_game").first.cards.create([
-	{name: "Amauris"},
-	{name: "Donovan"},
-	{name: "Chris"},
-	{name: "Jerry"},
-	{name: "Kim"},
-	{name: "Ohad"},
-	{name: "Quinn"},
-	{name: "Kirsten"},
-	{name: "Alex"},
-	{name: "Mandisa"},
-	{name: "Juan"},
-	{name: "Kassim"},
-	{name: "Jay"},
-	{name: "Rick"},
-	{name: "Bill"},
-	{name: "Matt E"},
-	{name: "Sanja"},
-	{name: "Matt S"},
-	{name: "Amzad"},
-	{name: "Greg"},
-	{name: "Sam"},
-	{name: "Liana"},
-	{name: "Andy"},
-	{name: "Nial"}])
+Game.where(name: "first_game").first.cards.create([
+	{name: "Donovan", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/2.jpg")},
+	{name: "Chris", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/3.jpg")},
+	{name: "Jerry", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/4.jpg")},
+	{name: "Kim", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/5.jpg")},
+	{name: "Ohad", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/6.jpg")},
+	{name: "Quinn", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/7.jpg")},
+	{name: "Kirsten", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/8.jpg")},
+	{name: "Alex", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/9.jpg")},
+	{name: "Mandisa", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/10.jpg")},
+	{name: "Juan", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/11.jpg")},
+	{name: "Kassim", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/12.jpg")},
+	{name: "Jay", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/13.jpg")},
+	{name: "Rick", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/14.jpg")},
+	{name: "Bill", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/15.jpg")},
+	{name: "Matt E", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/16.jpg")},
+	{name: "Sanja", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/17.jpg")},
+	{name: "Matt S", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/18.jpg")},
+	{name: "Amzad", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/19.jpg")},
+	{name: "Greg", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/20.jpg")},
+	{name: "Sam", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/21.jpg")},
+	{name: "Liana", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/22.jpg")},
+	{name: "Andy", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/23.jpg")},
+	{name: "Nial", avatar: File.new("C:/Users/Amauris/Desktop/nycda/week8/day5/instawho/app/assets/images/24.jpg")}])
 
 Tag.create([
-	{description:"dark hair"},
+	{description:"brown hair"},
 	{description:"red hair"},
 	{description:"blonde hair"},
 	{description:"bald"},
 	{description:"long hair"},
-	{description:"medium hair"},
+	{description:"medium-length hair"},
 	{description:"short hair"},
 	{description:"beard"},
 	{description:"mustache"},
-	{description:"clean shaven"},
-	{description:"dark eyes"},
+	{description:"no facial hair"},
+	{description:"brown eyes"},
 	{description:"green eyes"},
 	{description:"blue eyes"},
-	{description:"colored contacts"},
-	{description:"dark skin"},
-	{description:"brown skin"},
+	{description:"dark/brown skin"},
 	{description:"light skin"},
-	{description:"has glasses"},
-	{description:"no glasses"},
+	{description:"wears glasses"},
 	{description:"male"},
 	{description:"female"},
-	{description:"is tall"},
-	{description:"is short"},
+	{description:"taller than 5'7"},
 	{description:"pc computer"},
 	{description:"mac computer"},
 	{description:"younger than 30"},
-	{description:"older than 30"},
 	{description:"native new yorker"},
 	{description:"single"},
+	{description:"in a relationship"},
 	{description:"married"},
-	{description:"likes pepperoni pizza"},
-	{description:"likes cheese pizza"},
-	{description:"has earrings"},
-	{description:"does not have earrings"}])
+	{description:"prefers pepperoni pizza to cheese"},
+	{description:"has earrings or gauges"},
+	{description:"silver-grey hair"},
+	{description:"hazel eyes"},
+	{description:"black hair"},
+	{description:"has tattoos"},
+	{description:"prefers Backstreet boys to N'Sync"},
+	{description:"prefers Coke to Pepsi"},
+	{description:"not a soda person"}
+	])
 
 
-Card.where(name: "Amauris").first.tags << [Tag.find(1), Tag.find(6), Tag.find(10), Tag.find(11), Tag.find(17), Tag.find(19), Tag.find(20), Tag.find(22), Tag.find(24), Tag.find(26), Tag.find(29), Tag.find(32), Tag.find(34), Tag.find(28)]
+def add_tags(cardname, all_tags = [])
+	all_tags.each do |trait|
+		Card.where(name: cardname).first.tags << Tag.where(description: trait).first
+	end
+end
 
-Card.where(name: "Donovan").first.tags << [Tag.find(1), Tag.find(7), Tag.find(10), Tag.find(11), Tag.find(15), Tag.find(19), Tag.find(20), Tag.find(22), Tag.find(25), Tag.find(26), Tag.find(29), Tag.find(31), Tag.find(34), Tag.find(28)]
+add_tags("Amauris", ["black hair", "medium-length hair", "no facial hair", "brown eyes", "light skin", "male", "taller than 5'7", "pc computer", "younger than 30", "native new yorker", "in a relationship"])
 
-Card.where(name: "Chris").first.tags << [Tag.find(1), Tag.find(6), Tag.find(10), Tag.find(11), Tag.find(17), Tag.find(19), Tag.find(20), Tag.find(22), Tag.find(25), Tag.find(26), Tag.find(29), Tag.find(32), Tag.find(34), Tag.find(28)]
+add_tags("Donovan", ["black hair", "short hair", "no facial hair", "brown eyes", "dark/brown skin", "male", "taller than 5'7", "mac computer", "younger than 30", "native new yorker", "in a relationship", "prefers pepperoni pizza to cheese", "prefers Coke to Pepsi"])
 
-Card.where(name: "Jerry").first.tags << [Tag.find(1), Tag.find(6), Tag.find(10), Tag.find(11), Tag.find(17), Tag.find(18), Tag.find(20), Tag.find(23), Tag.find(24), Tag.find(26), Tag.find(29), Tag.find(31), Tag.find(34), Tag.find(28)]
+add_tags("Chris", ["black hair", "medium-length hair", "no facial hair", "brown eyes", "light skin", "male", "taller than 5'7", "mac computer", "younger than 30", "native new yorker", "single", "prefers pepperoni pizza to cheese", "prefers Coke to Pepsi"])
 
-Card.where(name: "Kim").first.tags << [Tag.find(1), Tag.find(5), Tag.find(10), Tag.find(11), Tag.find(17), Tag.find(18), Tag.find(21), Tag.find(23), Tag.find(25), Tag.find(27), Tag.find(30), Tag.find(32), Tag.find(33)]
+add_tags("Jerry", ["black hair", "medium-length hair", "beard", "mustache", "brown eyes", "light skin", "male", "pc computer", "younger than 30", "single", "wears glasses"])
 
-Card.where(name: "Ohad").first.tags << [Tag.find(1), Tag.find(6), Tag.find(10), Tag.find(12), Tag.find(17), Tag.find(18), Tag.find(20), Tag.find(22), Tag.find(25), Tag.find(26), Tag.find(30), Tag.find(31), Tag.find(34)]
+add_tags("Kim", ["black hair", "long hair", "no facial hair", "brown eyes", "light skin", "female", "mac computer", "younger than 30", "married", "prefers pepperoni pizza to cheese", "prefers Coke to Pepsi", "has earrings or gauges", "wears glasses"])
 
-Card.where(name: "Quinn").first.tags << [Tag.find(1), Tag.find(7), Tag.find(8), Tag.find(11), Tag.find(16), Tag.find(19), Tag.find(20), Tag.find(22), Tag.find(25), Tag.find(27), Tag.find(30), Tag.find(32), Tag.find(34), Tag.find(28)]
+add_tags("Ohad", ["brown hair", "medium-length hair", "beard", "mustache", "blue eyes", "light skin", "male", "mac computer", "taller than 5'7", "younger than 30", "married", "prefers pepperoni pizza to cheese", "not a soda person", "wears glasses"])
 
-Card.where(name: "Kirsten").first.tags << [Tag.find(1), Tag.find(5), Tag.find(10), Tag.find(11), Tag.find(17), Tag.find(19), Tag.find(21), Tag.find(23), Tag.find(25), Tag.find(26), Tag.find(29), Tag.find(31), Tag.find(33), Tag.find(28)]
+add_tags("Quinn", ["black hair", "short hair", "beard", "brown eyes", "dark/brown skin", "male", "taller than 5'7", "mac computer", "married", "prefers Coke to Pepsi"])
 
-Card.where(name: "Alex").first.tags << [Tag.find(1), Tag.find(7), Tag.find(10), Tag.find(11), Tag.find(17), Tag.find(19), Tag.find(20), Tag.find(22), Tag.find(25), Tag.find(26), Tag.find(29), Tag.find(32), Tag.find(34), Tag.find(28)]
+add_tags("Kirsten", ["brown hair", "long hair", "no facial hair", "brown eyes", "light skin", "female", "mac computer", "younger than 30", "single", "native new yorker", "prefers pepperoni pizza to cheese", "prefers Coke to Pepsi", "has earrings or gauges", "has tattoos", "prefers Backstreet boys to N'Sync"])
 
-Card.where(name: "Mandisa").first.tags << [Tag.find(1), Tag.find(5), Tag.find(10), Tag.find(11), Tag.find(15), Tag.find(19), Tag.find(21), Tag.find(23), Tag.find(25), Tag.find(26), Tag.find(29), Tag.find(31), Tag.find(33)]
+add_tags("Alex", ["black hair", "short hair", "no facial hair", "brown eyes", "light skin", "male", "mac computer", "taller than 5'7", "younger than 30", "single", "prefers pepperoni pizza to cheese", "not a soda person"])
 
-Card.where(name: "Juan").first.tags << [Tag.find(1), Tag.find(7), Tag.find(10), Tag.find(11), Tag.find(16), Tag.find(18), Tag.find(20), Tag.find(22), Tag.find(24), Tag.find(26), Tag.find(29), Tag.find(32), Tag.find(34)]
+add_tags("Mandisa", ["black hair", "long hair", "no facial hair", "brown eyes", "dark/brown skin", "female", "mac computer", "younger than 30", "single", "not a soda person"])
 
-Card.where(name: "Kassim").first.tags << [Tag.find(1), Tag.find(7), Tag.find(10), Tag.find(11), Tag.find(16), Tag.find(19), Tag.find(20), Tag.find(22), Tag.find(25), Tag.find(26), Tag.find(29), Tag.find(31), Tag.find(33)]
+add_tags("Juan", ["black hair", "short hair", "no facial hair", "brown eyes", "dark/brown skin", "male", "taller than 5'7", "pc computer", "single", "prefers Coke to Pepsi", "younger than 30", "wears glasses"])
 
-Card.where(name: "Jay").first.tags << [Tag.find(1), Tag.find(6), Tag.find(9), Tag.find(11), Tag.find(17), Tag.find(19), Tag.find(20), Tag.find(22), Tag.find(24), Tag.find(26), Tag.find(29), Tag.find(32), Tag.find(33), Tag.find(28)]
+add_tags("Kassim", ["black hair", "short hair", "no facial hair", "brown eyes", "dark/brown skin", "male", "taller than 5'7", "mac computer", "married", "prefers Coke to Pepsi", "younger than 30", "has earrings or gauges", "prefers Backstreet boys to N'Sync"])
 
-Card.where(name: "Rick").first.tags << [Tag.find(35), Tag.find(6), Tag.find(10), Tag.find(12), Tag.find(17), Tag.find(18), Tag.find(20), Tag.find(22), Tag.find(24), Tag.find(27), Tag.find(30), Tag.find(31), Tag.find(34), Tag.find(28)]
+add_tags("Jay", ["black hair", "medium-length hair", "mustache", "brown eyes", "light skin", "male", "taller than 5'7", "pc computer", "single", "younger than 30", "has earrings or gauges"])
 
-Card.where(name: "Bill").first.tags << [Tag.find(1), Tag.find(6), Tag.find(10), Tag.find(11), Tag.find(17), Tag.find(19), Tag.find(20), Tag.find(22), Tag.find(25), Tag.find(26), Tag.find(30), Tag.find(32), Tag.find(34), Tag.find(28)]
+add_tags("Rick", ["silver-grey hair", "medium-length hair", "no facial hair", "brown eyes", "light skin", "male", "taller than 5'7", "mac computer", "married", "native new yorker", "prefers Coke to Pepsi", "wears glasses"])
 
-Card.where(name: "Matt E").first.tags << [Tag.find(2), Tag.find(4), Tag.find(8), Tag.find(9), Tag.find(11), Tag.find(17), Tag.find(19), Tag.find(20), Tag.find(22), Tag.find(25), Tag.find(27), Tag.find(29), Tag.find(31), Tag.find(34), Tag.find(28)]
+add_tags("Bill", ["brown hair", "medium-length hair", "no facial hair", "brown eyes", "light skin", "male", "taller than 5'7", "mac computer", "native new yorker", "married", "not a soda person"])
 
-Card.where(name: "Sanja").first.tags << [Tag.find(1), Tag.find(5), Tag.find(10), Tag.find(11), Tag.find(17), Tag.find(18), Tag.find(21), Tag.find(23), Tag.find(25), Tag.find(27), Tag.find(30), Tag.find(32), Tag.find(33)]
+add_tags("Matt E", ["blonde hair", "bald", "beard", "mustache", "hazel eyes", "light skin", "male", "taller than 5'7", "mac computer", "native new yorker", "in a relationship", "has tattoos"])
 
-Card.where(name: "Matt S").first.tags << [Tag.find(1), Tag.find(5), Tag.find(8), Tag.find(9), Tag.find(11), Tag.find(17), Tag.find(19), Tag.find(20), Tag.find(22), Tag.find(25), Tag.find(27), Tag.find(29), Tag.find(31), Tag.find(34), Tag.find(28)]
+add_tags("Sanja", ["brown hair", "long hair", "no facial hair", "brown eyes", "light skin", "female", "mac computer", "married", "prefers Coke to Pepsi", "has earrings or gauges", "wears glasses", "prefers Backstreet boys to N'Sync"])
 
-Card.where(name: "Amzad").first.tags << [Tag.find(1), Tag.find(6), Tag.find(10), Tag.find(11), Tag.find(16), Tag.find(19), Tag.find(20), Tag.find(23), Tag.find(25), Tag.find(26), Tag.find(29), Tag.find(31), Tag.find(34)]
+add_tags("Matt S", ["brown hair", "medium-length hair", "beard", "mustache", "brown eyes", "light skin", "male", "taller than 5'7", "mac computer", "in a relationship", "prefers Coke to Pepsi", "prefers pepperoni pizza to cheese", "prefers Backstreet boys to N'Sync"])
 
-Card.where(name: "Greg").first.tags << [Tag.find(1), Tag.find(7), Tag.find(10), Tag.find(11), Tag.find(16), Tag.find(19), Tag.find(20), Tag.find(22), Tag.find(25), Tag.find(27), Tag.find(29), Tag.find(32), Tag.find(34), Tag.find(28)]
+add_tags("Amzad", ["black hair", "medium-length hair", "no facial hair", "brown eyes", "dark/brown skin", "male", "mac computer", "married", "prefers Coke to Pepsi", "younger than 30", "prefers Backstreet boys to N'Sync"])
 
-Card.where(name: "Sam").first.tags << [Tag.find(1), Tag.find(6), Tag.find(10), Tag.find(12), Tag.find(17), Tag.find(19), Tag.find(20), Tag.find(22), Tag.find(25), Tag.find(26), Tag.find(29), Tag.find(31), Tag.find(33), Tag.find(28)]
+add_tags("Greg", ["black hair", "medium-length hair", "no facial hair", "brown eyes", "dark/brown skin", "male", "mac computer", "taller than 5'7", "in a relationship", "prefers pepperoni pizza to cheese", "prefers Coke to Pepsi", "younger than 30"])
 
-Card.where(name: "Liana").first.tags << [Tag.find(1), Tag.find(5), Tag.find(10), Tag.find(11), Tag.find(17), Tag.find(19), Tag.find(21), Tag.find(23), Tag.find(25), Tag.find(27), Tag.find(29), Tag.find(32), Tag.find(33)]
+add_tags("Sam", ["brown hair", "medium-length hair", "no facial hair", "blue eyes", "light skin", "male", "taller than 5'7", "mac computer", "younger than 30", "native new yorker", "in a relationship", "prefers pepperoni pizza to cheese", "has earrings or gauges", "has tattoos", "not a soda person"])
 
-Card.where(name: "Andy").first.tags << [Tag.find(3), Tag.find(6), Tag.find(8), Tag.find(9), Tag.find(12), Tag.find(17), Tag.find(18), Tag.find(20), Tag.find(22), Tag.find(25), Tag.find(26), Tag.find(29), Tag.find(31), Tag.find(34), Tag.find(28)]
+add_tags("Liana", ["brown hair", "long hair", "no facial hair", "blue eyes", "light skin", "female", "mac computer", "in a relationship", "not a soda person", "has earrings or gauges", "has tattoos", "native new yorker"])
 
+add_tags("Andy", ["blonde hair", "medium-length hair", "beard", "mustache", "blue eyes", "light skin", "male", "taller than 5'7", "mac computer", "younger than 30", "single", "not a soda person"])
+
+add_tags("Niall", ["blonde hair", "medium-length hair", "no facial hair", "green eyes", "light skin", "male", "taller than 5'7", "mac computer", "younger than 30", "in a relationship", "native new yorker", "prefers Coke to Pepsi"])
