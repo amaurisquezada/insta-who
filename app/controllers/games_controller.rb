a3c9743197f7431cd320675fd0f30ca342d223d9
Christopher <chan42988@gmail.com>
2016-06-02 20:06:58 -0400

    pushing styling

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,7 +11,7 @@ class GamesController < ApplicationController
   def show
   	@mygame = Game.find(1)
   	@mycard = @mygame.cards.sample
-    @myuser = User.where(username: "aq").first
+    @myuser = User.where(username: "ch123").first
   	MysteryCard.where(user_id: @myuser.id, game_id: @mygame.id).destroy_all
   	MysteryCard.create(user_id: @myuser.id, game_id: @mygame.id, card_name: @mycard.name, counter: 0)
     @available_tags = @mygame.cards.map{|card| card.tags.map{|tag| tag.description}}.flatten.uniq
@@ -19,7 +19,7 @@ class GamesController < ApplicationController
 
   def update_card_position
   	@mygame = Game.find(1)
-    @myuser = User.where(username: "aq").first
+    @myuser = User.where(username: "ch123").first
   	@mysterycard = MysteryCard.where(user_id: @myuser.id, game_id: @mygame.id).last
   	@mysterycard.update(counter: @mysterycard.counter+1)
   	@counter = @mysterycard.counter
@@ -33,7 +33,7 @@ class GamesController < ApplicationController
 
   def play_again  	
   	@mygame = Game.find(1)
-    @myuser = User.where(username: "aq").first
+    @myuser = User.where(username: "ch123").first
   	MysteryCard.where(user_id: @myuser.id, game_id: @mygame.id).last.destroy
   	@mycard = @mygame.cards.sample
   	MysteryCard.create(user_id: @myuser.id, game_id: @mygame.id, card_name: @mycard.name, counter: 0)
