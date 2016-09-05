40d4b90b3db6c1a2b3985023da28962caea14ad7
Amauris Quezada <Amauris.Quezada@gmail.com>
2016-09-05 16:39:06 -0400

    adjusting css on game

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,21 +1,10 @@
-	$( window ).load(function() {
-	var game_cards = $(".one-card-container")
-	var separate_mystery_card = $(".selected")
-	var game_card_width = game_cards.width()
-	game_cards.css('height', game_card_width)
-	separate_mystery_card.css('height', game_card_width) 
-	 
-});
+	function gameSizer() {
+		$(".one-card-container").css('height', $(".one-card-container").width());
+		$(".selected").css('height', $(".one-card-container").width());
+};
 
-
-
-$( window ).resize(function() {
-	var game_cards = $(".one-card-container")
-	var separate_mystery_card = $(".selected")
-	var game_card_width = game_cards.width()
-	game_cards.css('height', game_card_width)
-	separate_mystery_card.css('height', game_card_width)  
-});
+$( window ).load(gameSizer);
+$( window ).resize(gameSizer);
 
 
 
