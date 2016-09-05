9e84f0c3103c2ff39f681f8f779e2805e90f2dc0
Amauris Quezada <Amauris.Quezada@gmail.com>
2016-09-05 18:31:38 -0400

    fixing randomizer

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -3,6 +3,8 @@
 		$(".selected").css('height', $(".one-card-container").width());
 		var rW = $(window).width() - ($(".one-card-container").width()* 6) - $("#right-wrapper").width();
 		$("#right-wrapper").css("right", rW/2);
+		$(".howto").css("top", $(".one-card-container").height()*4 + 20)
+		$(".warning").css("top", $(".one-card-container").height()*4 + 55)
 };
 
 $( window ).load(gameSizer);
