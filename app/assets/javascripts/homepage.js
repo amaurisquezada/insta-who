9baea50ebecdffb9c9b65c03183df8c66b2824b8
Amauris Quezada <Amauris.Quezada@gmail.com>
2016-09-05 15:42:36 -0400

    changing startup button

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,22 +1,25 @@
 $(document).ready(function(){
-	$('#signup-toggle-button').click(function(){
-			$('.login-form').css('display', 'block');
-			$('.backdrop').css('display', 'block');
-			$('.backdrop').css('opacity', '.6');
-	})
+	// $('#signup-toggle-button').click(function(){
+	// 		$('.login-form').css('display', 'block');
+	// 		$('.backdrop').css('display', 'block');
+	// 		$('.backdrop').css('opacity', '.6');
+	// })
 
-	$('.backdrop').click(function(){
-			$('.login-form').css('display', 'none');
-			$('.backdrop').css('display', 'none');
-	})
+	// $('.backdrop').click(function(){
+	// 		$('.login-form').css('display', 'none');
+	// 		$('.backdrop').css('display', 'none');
+	// })
 
-	function homepageCenter() {
-		$("#signup-toggle-button").css("margin-left", ($(window).width() - $("#signup-toggle-button").width() - 190)/2 )
+	// function homepageCenter() {
+	// 	var mL = ($(window).width() - $(".button_to input").width() - 190)/2;
+	// 	var mT = ($(window).height() - $(".button_to input").height())/2;
+	// 	$(".button_to input").css("left", mL)
+	// 	$(".button_to input").css("top", mT)
+	// 	$("#hb-caption").css("left", mL+($(".button_to input").width()/2) + 95)
+	// 	$("#hb-caption").css("top", mT+($(".button_to input").height()/2) + 50)
+	// }
 
-		$("#signup-toggle-button").css("margin-top", ($(window).height() - $("#signup-toggle-button").height())/2 )
-	}
-
-	homepageCenter();
-	$(window).resize(homepageCenter)
+	// homepageCenter();
+	// $(window).resize(homepageCenter)
 
 });
--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-	console.log('working');
 
 	$('#main-nav-container').hide();
 
