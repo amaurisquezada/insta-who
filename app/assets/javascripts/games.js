a6b8fca046f75011e3095582f02ef654b52fa189
Amauris Quezada <Amauris.Quezada@gmail.com>
2016-09-05 17:19:54 -0400

    adjusting game spacing

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,6 +1,8 @@
 	function gameSizer() {
-		$(".one-card-container").css('height', $(".one-card-container").width());
+		$(".one-card-container").css('height', $(".one-card-container").width() - 6);
 		$(".selected").css('height', $(".one-card-container").width());
+		var rW = $(window).width() - ($(".one-card-container").width()* 6) - $("#right-wrapper").width();
+		$("#right-wrapper").css("right", rW/2);
 };
 
 $( window ).load(gameSizer);
