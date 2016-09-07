66257679764e86b896a4fee330d597f83eba619c
Amauris Quezada <Amauris.Quezada@gmail.com>
2016-09-07 18:45:00 -0400

    adjusting game look

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,10 +1,14 @@
 	function gameSizer() {
 		$(".one-card-container").css('height', $(".one-card-container").width() - 6);
-		$(".selected").css('height', $(".one-card-container").width());
+		$(".selected").css('width', $(".one-card-container").width());
+		$(".selected").css('height', $(".selected").width());
+		$("#winning-card").css('width', $(".one-card-container").width());
+		$("#winning-card").css('height', $(".selected").width());
 		var rW = $(window).width() - ($(".one-card-container").width()* 6) - $("#right-wrapper").width();
 		$("#right-wrapper").css("right", rW/2);
-		$(".howto").css("top", $(".one-card-container").height()*4 + 20)
-		$(".warning").css("top", $(".one-card-container").height()*4 + 60)
+		$(".howto").css("top", $(".one-card-container").height()*4 + 32)
+		$(".warning").css("top", $(".one-card-container").height()*4 + 70)
+
 };
 
 $( window ).load(gameSizer);
--- app/assets/javascripts/user_test.js
+++ app/assets/javascripts/user_test.js
@@ -2,7 +2,13 @@
     el.css('background', 'green');
   };
 
-  non_match = function(el){
-    el.css('-webkit-transform', 'rotateX(-180deg)');
-    el.css('transform', 'rotateX(-180deg)');
+  function non_match(el){
+  			$(".selected").css("border-color", "green")
+        el.css("border", "2px solid red")
+        setTimeout(function(){
+        	 el.css('-webkit-transform', 'rotateX(-180deg)');
+   				 el.css('transform', 'rotateX(-180deg)');
+   				 $(".selected").css("border-color", "black")
+   				 el.children().css("border", "black solid 2px")
+        }, 1000);   
   };
