673d6a4f603d31eb78f04cc60b66d8c19a803588
Amauris Quezada <Amauris.Quezada@gmail.com>
2016-09-07 23:29:56 -0400

    resizing winning card

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -3,12 +3,10 @@
 		$(".one-card-container").css('height', cardWidth - (cardWidth/31));
 		$(".selected").css('width', cardWidth);
 		$(".selected").css('height', cardWidth - (cardWidth/31));
-		$("#winning-card").css('width', cardWidth);
-		$("#winning-card").css('height', cardWidth - (cardWidth/31));
+		$("#winning_card").css('width', cardWidth);
+		$("#winning_card").css('height', cardWidth - (cardWidth/31));
 		$(".one-card-container").css('border-width', cardWidth/31)
 		$(".card_names").css("font-size", cardWidth/8)
-		$("#winning-card").css('width', cardWidth);
-		$("#winning-card").css('height', cardWidth);
 		$("#all-guesses").css("width", cardWidth)
 		$(".indent").css("margin-right", ($("#all-guesses").width()-($(".indent").width() * 5))/4 - 3)
 		var rW = $(window).width() - (cardWidth* 6) - $("#right-wrapper").width();
